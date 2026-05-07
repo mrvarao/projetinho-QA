140ab351471e38a3d4778a68fd7cec9bbedc1492
juninhplay99 <juninhplay99@gmail.com>
2026-05-06 22:25:06 -0300

    aplica scroll e fallback de click tambem nos botoes de checkout

--- web-tests/pages/cart_page.py
+++ web-tests/pages/cart_page.py
@@ -1,3 +1,4 @@
+from selenium.common.exceptions import ElementClickInterceptedException, WebDriverException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
@@ -11,12 +12,20 @@ class CartPage:
         self.driver = driver
 
     def get_cart_items(self):
-        WebDriverWait(self.driver, 15).until(
+        WebDriverWait(self.driver, 20).until(
             EC.presence_of_all_elements_located(self.CART_ITEMS)
         )
         return [el.text for el in self.driver.find_elements(*self.CART_ITEMS)]
 
     def proceed_to_checkout(self):
-        wait = WebDriverWait(self.driver, 15)
-        wait.until(EC.element_to_be_clickable(self.CHECKOUT_BUTTON)).click()
+        wait = WebDriverWait(self.driver, 20)
+        button = wait.until(EC.presence_of_element_located(self.CHECKOUT_BUTTON))
+        self.driver.execute_script(
+            "arguments[0].scrollIntoView({block: 'center'});", button
+        )
+        wait.until(EC.element_to_be_clickable(self.CHECKOUT_BUTTON))
+        try:
+            button.click()
+        except (ElementClickInterceptedException, WebDriverException):
+            self.driver.execute_script("arguments[0].click();", button)
         wait.until(EC.url_contains("checkout-step-one"))
--- web-tests/pages/checkout_page.py
+++ web-tests/pages/checkout_page.py
@@ -1,3 +1,4 @@
+from selenium.common.exceptions import ElementClickInterceptedException, WebDriverException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
@@ -14,24 +15,34 @@ class CheckoutPage:
     def __init__(self, driver):
         self.driver = driver
 
+    def _click(self, locator):
+        wait = WebDriverWait(self.driver, 20)
+        button = wait.until(EC.presence_of_element_located(locator))
+        self.driver.execute_script(
+            "arguments[0].scrollIntoView({block: 'center'});", button
+        )
+        wait.until(EC.element_to_be_clickable(locator))
+        try:
+            button.click()
+        except (ElementClickInterceptedException, WebDriverException):
+            self.driver.execute_script("arguments[0].click();", button)
+
     def fill_info(self, first_name, last_name, zip_code):
-        wait = WebDriverWait(self.driver, 15)
+        wait = WebDriverWait(self.driver, 20)
         first = wait.until(EC.visibility_of_element_located(self.FIRST_NAME_INPUT))
         first.send_keys(first_name)
         self.driver.find_element(*self.LAST_NAME_INPUT).send_keys(last_name)
         self.driver.find_element(*self.ZIP_CODE_INPUT).send_keys(zip_code)
 
     def continue_checkout(self):
-        wait = WebDriverWait(self.driver, 15)
-        wait.until(EC.element_to_be_clickable(self.CONTINUE_BUTTON)).click()
-        wait.until(EC.url_contains("checkout-step-two"))
+        self._click(self.CONTINUE_BUTTON)
+        WebDriverWait(self.driver, 20).until(EC.url_contains("checkout-step-two"))
 
     def finish_checkout(self):
-        wait = WebDriverWait(self.driver, 15)
-        wait.until(EC.element_to_be_clickable(self.FINISH_BUTTON)).click()
-        wait.until(EC.url_contains("checkout-complete"))
+        self._click(self.FINISH_BUTTON)
+        WebDriverWait(self.driver, 20).until(EC.url_contains("checkout-complete"))
 
     def get_confirmation_message(self):
-        return WebDriverWait(self.driver, 15).until(
+        return WebDriverWait(self.driver, 20).until(
             EC.visibility_of_element_located(self.CONFIRMATION_HEADER)
         ).text
